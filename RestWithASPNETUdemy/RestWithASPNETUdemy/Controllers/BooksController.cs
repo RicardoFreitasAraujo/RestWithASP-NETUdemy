a26eda05d78a6efbf7fe901200f009a566ba46a6
Your Name <you@example.com>
2019-08-21 10:16:03 -0300

    Generic Repository

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/BooksController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Business.Implementation;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private IPersonBusiness _bookBusiness;
+
+        public BooksController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(this._bookBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = this._bookBusiness.FindById(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(this._bookBusiness.Create(person));
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Book book)
+        {
+            if (book == null) return NotFound();
+            //return Ok(this._bookBusiness.Update(person));
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _bookBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Base/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace RestWithASPNETUdemy.Model.Base
+{
+    [DataContract]
+    public class BaseEntity
+    {
+        [DataMember]
+        public long Id { get; set; }
+    }
+}
