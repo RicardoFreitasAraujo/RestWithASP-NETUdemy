b79327747e21f7af4c576bfa3d256cbc0af8c07c
Your Name <you@example.com>
2019-08-21 16:39:19 -0300

    DataContract

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/BookVO.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/BookVO.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace RestWithASPNETUdemy.Data.VO
 {
+    [DataContract]
     public class BookVO
     {
+        [DataMember(Order = 1 , Name = "Codigo")]
         public long Id { get; set; }
+        [DataMember(Order = 2, Name = "Title")]
         public string Title { get; set; }
+        [DataMember(Order = 3, Name = "Author")]
         public string Author { get; set; }
+        [DataMember(Order = 4, Name = "Price")]
         public decimal? Price { get; set; }
+        [DataMember(Order = 5, Name = "LaunchDate")]
         public DateTime? LaunchDate { get; set; }
     }
 }
